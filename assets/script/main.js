bcc9cc348061fcac50169b64e72e16e8c4bcec04
Romeu Reif <romeureif10@gmail.com>
2023-11-16 15:47:16 -0300

    func verificarPartida

--- assets/script/main.js
+++ assets/script/main.js
@@ -25,17 +25,32 @@ for(let i= 0; i < emojis.length; i++){
     let caixa = document.createElement("div");
     caixa.className = "item";
     caixa.innerHTML = embaralharEmojis[i];
-}
+};
 
 function click(){
     if(abrirCartas.length < 2){
-        this.classList.add("caixaAbrir");
-        abrirCartas.push(this)
+        this.classList.add("caixaAberta");
+        abrirCartas.push(this);
     }
 
     if(abrirCartas == 2){
-        setTimeout(verificarPartida, 500)
-    }
+        setTimeout(verificarPartida, 500);
+    };
 
     console.log(abrirCartas);
 }
+
+function verificarPartida(){
+    if(abrirCartas[0].innerHTML === abrirCartas[1].innerHTML){
+        abrirCartas[0].classList.add("caixaCerta");
+        abrirCartas[1].classList.add("caixaCerta");
+    } else{
+        abrirCartas[0].classList.remove("caixaAberta");
+        abrirCartas[1].classList.remove("caixaAberta");
+    };
+
+    abrirCartas = [];
+    if(document.querySelectorAll(".caixaCerta").length === emojis.length){
+        alert("Você VENCEU!");
+    };
+};
