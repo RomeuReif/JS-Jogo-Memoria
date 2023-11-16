8b8d8aadd3957485c8e36628cdb66b7d9a28320c
Romeu Reif <romeureif10@gmail.com>
2023-11-16 15:42:54 -0300

    func click

--- assets/script/main.js
+++ assets/script/main.js
@@ -20,3 +20,22 @@ const emojis = [
 let abrirCartas = [];
 
 let embaralharEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i= 0; i < emojis.length; i++){
+    let caixa = document.createElement("div");
+    caixa.className = "item";
+    caixa.innerHTML = embaralharEmojis[i];
+}
+
+function click(){
+    if(abrirCartas.length < 2){
+        this.classList.add("caixaAbrir");
+        abrirCartas.push(this)
+    }
+
+    if(abrirCartas == 2){
+        setTimeout(verificarPartida, 500)
+    }
+
+    console.log(abrirCartas);
+}
